198062c5165f21cdc2b80b8dcb90591e56a20d89
KaseyKalka <kaseykalka@gmail.com>
2023-06-28 19:34:54 -0500

    fixed handleSubmit and created useState for pokemonName

--- app/src/components/PokemonCard/PokemonCard.tsx
+++ app/src/components/PokemonCard/PokemonCard.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useEffect, useRef, useState } from 'react'
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
 
 interface Pokemon {
             id: number,
@@ -9,15 +9,20 @@ interface Pokemon {
 
 const PokemonCard = () => {
 
-        const inputRef = useRef<HTMLInputElement>(null)
+        //const inputRef = useRef<HTMLInputElement>(null)
       
         const handleSubmit = (event: FormEvent) => {
-          if (inputRef.current !== null) {
             event.preventDefault()
-            let pokemonName = inputRef.current.value
-            return pokemonName
-          }
+            console.log(pokemonName)
+            getPokemon()
         }
+
+        // const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        //     setPokemonName(event.target.value)
+        //     console.log(pokemonName)
+        // }
+
+        const [pokemonName, setPokemonName] = useState<string>('')
         
         const [pokemon, setPokemon] = useState<Pokemon>({
             id: 0,
@@ -26,17 +31,18 @@ const PokemonCard = () => {
             ability: ''
         })
 
+        const getPokemon = async () => {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+            const data = await response.json()
+            setPokemon({
+                id: data.id,
+                name: data.name,
+                img: data.sprites.front_default,
+                ability: data.abilities[0].ability.name
+            })
+        }
+
         useEffect(() => {
-            const getPokemon = async () => {
-                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/{pokemonName}`)
-                const data = await response.json()
-                setPokemon({
-                    id: data.id,
-                    name: data.name,
-                    img: data.sprints.front_default,
-                    ability: data.abilties[0].ability.name
-                })
-            }
             getPokemon()
         }, [])
 
@@ -45,18 +51,18 @@ const PokemonCard = () => {
             <div >
               <form className='form' onSubmit={handleSubmit}>
                 <label htmlFor="todo">Search for a Pokemon </label><br />
-                <input type="text" ref={inputRef}/>
+                <input type="text" value={pokemonName} onChange={(event) => setPokemonName(event.target.value)}/>
                 <button type="submit">Search</button>
               </form>
             </div>
 
             <div className="card" style={{width: '18rem'}}>
-                <img src="{pokemon.img}" className="card-img-top" alt="..." />
+                <img src={pokemon.img} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{pokemon.name}</h5>
                 </div>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item">{pokemon.id}</li>
+                    <li className="list-group-item">Pokemon Number: {pokemon.id}</li>
                     <li className="list-group-item">{pokemon.ability}</li>
                 </ul>
             </div>
